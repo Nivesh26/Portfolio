813c9ca75499c98ff71f248c145ce4682e2e9c48
Nivesh26 <nivesh.shrestha.94@gmail.com>
2025-11-11 13:44:16 +0545

    added: added the edits

---
+++ src/types/react-scroll.d.ts
@@ -0,0 +1,3 @@
+// Minimal type shim for 'react-scroll' to satisfy TypeScript during build.
+// If you need richer typings, install @types/react-scroll or write full definitions.
+declare module 'react-scroll';
